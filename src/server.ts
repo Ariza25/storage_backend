a1a8cc39e5320e22073d078085095443d10b57b0
unknown <mjsa.sanches25@gmail.com>
2024-01-02 16:19:33 -0300

    lastcommit

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ const start = async () => {
     await app.register(routes)
 
         try{
-            await app.listen({port: 3333})
+            await app.listen({port: 3333 || process.env.PORT})
         }catch(err){
             process.exit(1)
         }
